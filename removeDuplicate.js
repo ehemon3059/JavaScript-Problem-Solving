a4a34f253e852137e2b936ecb061895b78d1f68d
Eh.Emon <eh.emon3059@gmail.com>
2024-06-21 21:12:13 +0600

    remove duplicate method 1

--- removeDuplicate.js
+++ removeDuplicate.js
@@ -1,1 +1,26 @@
 // Method 1 :
+//This is friend list
+const friends = ['Emon','korim','sunny','sunny','eMon','korim','Emu'];
+
+//now i have to remove duplicate name from this array
+//make a function to do this
+function removeDuplicate(names){
+    //make an empty array
+    const friendsName = [];
+
+    //create a loop to separate name
+    for(let i = 0; i<names.length; i++){
+        const name = names[i].toUpperCase();
+    
+        if(friendsName.includes(name) === false){
+            //name push on friendsName Array
+            friendsName.push(name)
+        }
+        
+
+    }
+    return friendsName;
+}
+
+const result = removeDuplicate(friends);
+console.log(result);
